4328336539ea357d96df4e29990671299131b044
Gabe Bravo <gabecbravo@gmail.com>
2020-11-22 15:18:54 -0500

    Added the basic setup to do flask_jwt auth using user data from sqlite db

--- security.py
+++ security.py
@@ -4,21 +4,10 @@ from werkzeug.security import safe_str_cmp
 from user import User  # syntax from <file> import <class>
 from typing import Union  # for potential multiple types
 
-# mock table of users
-users = [
-    User(1, 'bob', 'foo'),
-    User(2, 'jenny', 'bar'),
-    User(1, 'will', 'baz'),
-]
-
-# mapping functions : using set comprehensions
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
-
 
 # return a User or None
 def authenticate(username: str, password: str) -> Union[User, None]:
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
@@ -26,4 +15,4 @@ def authenticate(username: str, password: str) -> Union[User, None]:
 #  unique to Flask JWT - passes in JWT and extracts user_id
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
--- user.py
+++ user.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+import sqlite3
 from dataclasses import dataclass
+from typing import Union
 
 
 @dataclass
@@ -10,3 +13,41 @@ class User:
   # print our string object
     def __repr__(self):
         return f'<User id:{self.id}, username:{self.username}, password:{self.password}>'
+
+  # get user in sqlite by useranme
+    @classmethod  # denotes class method, self/User references = cls
+    def find_by_username(cls, username: str) -> Union[User, None]:
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'SELECT * FROM users WHERE username=?'  # ? syntax is for a param
+        # this is the param as a tuple
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+
+        if row:
+            user = cls(*row)  # spread operator
+        else:
+            user = None
+
+        connection.close()
+        return user
+
+  # get user in sqlite by id
+    @classmethod  # denotes class method, self/User references = cls
+    def find_by_id(cls, _id: int) -> Union[User, None]:
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = 'SELECT * FROM users WHERE id=?'  # ? syntax is for a param
+        # this is the param as a tuple
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+
+        if row:
+            user = cls(*row)  # spread operator
+        else:
+            user = None
+
+        connection.close()
+        return user
